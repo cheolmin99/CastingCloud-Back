707bb640559e308bff690aa39f380fd5a892d5c8
cheolmin99 <cjfals8570@naver.com>
2023-09-18 12:54:57 +0900

    데이터 베이스 연결

---
+++ src/main/java/com/example/castingCloud/CastingCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.castingCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastingCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastingCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/castingCloud/CastingCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.castingCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastingCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
